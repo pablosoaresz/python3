f2cd1f27bc7036210dbf0d66013073bd3f5be63d
pablosoaresz <pabloetf@gmail.com>
2020-01-17 17:04:07 -0400

    Message (commit on 'python3')

--- PythonExercicios/ex016.py
+++ PythonExercicios/ex016.py
@@ -1,8 +1,10 @@
 #Crie um programa que leia um número Real qualquer pelo teclado e mostre na tela a sua porção inteira
 #Ex: Digite um número: 6.127
 #O número 6.127 tem a parte inteira 6.
-#Use a biblioteca 'math'.
+#Use a biblioteca 'math.
 
-import math
+#import math
+from math import trunc
 num = float(input('Digite um número Real: '))
-print(math.trunc(num))
+print('O número digitado foi {}, e sua parte inteira é {}.'.format(num, trunc(num)))
+print('O número digitado foi {}, e a sua parte inteira é {} impresso usando "int".'.format(num, int(num)))
