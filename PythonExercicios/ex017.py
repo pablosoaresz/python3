06b28d94c2f318eff0c029eeb6a7617b4e628e85
pablosoaresz <pabloetf@gmail.com>
2020-01-17 17:16:27 -0400

    Message (commit python3)

--- PythonExercicios/ex017.py
+++ PythonExercicios/ex017.py
@@ -1,9 +1,12 @@
 #Faça um programa que leia o comprimento do cateto oposto e do cateto adjacente de um triângulo, calcule
 # e mostre o comprimento da hipotenusa
 
-import math
-
-catop = float(input('Digite o valor do Cateto Oposto: '))
-catadj = float(input('Digite o valo do Cateto Adjacente: '))
-hipot = math.hypot(catop, catadj)
-print(hipot)
+#import math
+from math import hypot
+catop = float(input('Comprimento do cateto oposto: '))
+catadj = float(input('Comprimento do cateto adjacente: '))
+#hipot = math.hypot(catop, catadj)
+hipot = hypot(catop, catadj)
+hipote = (catop ** 2 + catadj ** 2) ** (1/2)
+print('Usando a importação do método "hypot" do módulo "math" a hipotenusa vai medir {:.2f}.'.format(hipot))
+print('Usando o método matemático, a hipotenusa irá medir {:.2f}.'.format(hipote))
