699449555b09ba2f1bcc85ba253218344d6eb6c0
pablosoaresz <pabloetf@gmail.com>
2020-01-18 19:12:09 -0400

    aulas dia 18/01/2020

---
+++ PythonExercicios/exe023.py
@@ -0,0 +1,6 @@
+#Faça um programa que leia o número de 0 a 9999 e mostre na tela cada um dos dígitos separados
+#Ex: Digite um número: 1834
+#Unidade: 3
+#Dezena: 3
+#Centena: 8
+#Milhar: 1
---
+++ PythonExercicios/exe024.py
@@ -0,0 +1,1 @@
+#Cria um programa que leia o nome de uma cidade e diga se ela começa ou não com o nome "SANTO".
