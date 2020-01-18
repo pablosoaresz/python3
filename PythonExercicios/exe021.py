de2c894a7e9f309d934f0f772aa31d773a434217
pablosoaresz <pabloetf@gmail.com>
2020-01-17 20:00:11 -0400

    .

--- PythonExercicios/exe021.py
+++ PythonExercicios/exe021.py
@@ -1,7 +1,4 @@
 #Faça um programa em python que abra e reproduza o audio de um arquivo MP3.
 
 from playsound import playsound
-import pygame
 playsound('/Users/Ninho/PycharmProjects/PythonExercicios/ex021.mp3', True)
-pygame.init()
-
