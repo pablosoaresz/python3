a375c8555920d72b731f6344c68342b73b2d057e
pablosoaresz <pabloetf@gmail.com>
2020-01-18 22:33:15 -0400

    aulas dia 18/01/2020

--- PythonExercicios/exe026.py
+++ PythonExercicios/exe026.py
@@ -2,3 +2,8 @@
 #1- Quantas vezes aparece a letra "A".
 #2- Em que posição ela aparece a primeira vez
 #3- Em que posição ela aparece a última vez.
+
+frase = str(input('Digite uma frase: ')).strip().upper()
+print('Existem {} letras A na frase digitada.'.format(frase.count('A')))
+print('A posição onde a letra A apareceu pela primeira vez foi na: {}.'.format(frase.find('A')+1))
+print('A ultima posição onde a letra A apareceu foi: {}.'.format(frase.rfind('A')+1))
--- PythonExercicios/exe027.py
+++ PythonExercicios/exe027.py
@@ -2,3 +2,9 @@
 #Ex: Ana Maria de Souza
 #primeiro = Ana
 #último = Souza
+
+nome = str(input('Digite seu nome: ').strip())
+dividido = nome.split()
+print('O primeiro nome é: {}, e o ultimo nome é: {}.'.format(dividido[0], dividido[-1]))
+# OU
+print('O primeiro nome é: {}, e o último nome é: {}.'.format(dividido[0], dividido[len(dividido)-1]))
