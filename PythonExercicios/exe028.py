c9aea806ab6cee2f0cca1841d7c1278c28391f7f
pablosoaresz <pabloetf@gmail.com>
2020-01-19 19:12:20 -0400

    aulas dia 19/01/2020

--- PythonExercicios/exe028.py
+++ PythonExercicios/exe028.py
@@ -1,3 +1,12 @@
 #Escreva um programa que faça o computador "pensar" em um número inteiro entre 0 e 5 e peça para o usuário tentar descobrir
 #qual foi o número escolhido pelo computador.
 #O programa deverá escrever na tela se o usuário venceu ou perdeu.
+
+import random
+num = random.randrange(6)
+user_num = int(input('O computador irá "pensar" em um número entre 0 e 5, digite '
+                     '\no número que você acha que o computador pensou: '))
+if user_num == num:
+    print('Parabéns você GANHOU ao usar o número {}, e o computador pensou este número: {}.'.format(user_num, num))
+else:
+    print('Você PERDEU ao usar o número {}, o computador pensou no número: {}!'.format(user_num,num))
--- PythonExercicios/exe029.py
+++ PythonExercicios/exe029.py
@@ -1,3 +1,9 @@
 #Escreva um programa que leia a velocidade de um carro.
 #Se ele ultrapassar 80km/h, mostre uma mensagem dizendo que ele foi multado
 #A multa vai custar R$7,00 por cada KM/h acima do limite.
+velocidade = float(input('Digite a velocidade aferida: '))
+if velocidade > 80.0:
+    print('Você foi multado ao ultrapassar o limite de velocidade que é 80km/h. Sua velocidade aferida foi de: {:.2f}km/h,\n e sua multa será de R${:.2f} Reais.'
+          .format(velocidade, ((velocidade - 80.00) * 7.00)))
+else:
+    print('Segue a vida...')
