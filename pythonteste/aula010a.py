f03561c12addab4a005a07864e069414790d899f
pablosoaresz <pabloetf@gmail.com>
2020-01-19 15:30:23 -0400

    aulas dia 19/01/2020

---
+++ pythonteste/aula010a.py
@@ -0,0 +1,7 @@
+tempo = int(input('Quantos anos tem o seu carro? '))
+if tempo <= 3:
+    print('Carro novo')
+else:
+    print('Carro velho')
+print('Carro novo' if tempo <= 3 else'Carro Velho')
+print('---FIM---')
