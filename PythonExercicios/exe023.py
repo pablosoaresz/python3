b7a601c00a46c77b8a2e494cf2dcb3c106e0ee55
pablosoaresz <pabloetf@gmail.com>
2020-01-18 20:53:13 -0400

    aulas dia 18/01/2020

--- PythonExercicios/exe023.py
+++ PythonExercicios/exe023.py
@@ -4,3 +4,18 @@
 #Dezena: 3
 #Centena: 8
 #Milhar: 1
+
+num = int(input('Digite um número entre 0 a 9999: '))
+n = str(num)
+print( 'Analisando o número {}...' .format(num) )
+print( 'Unidade: {}.'.format(n[3]) , '\nDezena: {}.' .format(n[2]) ,
+        '\nCentena: {}' .format(n[1]) , '\nMilhar: {}.' .format(n[0]) )
+u = num // 1 % 10
+d = num // 10 % 10
+c = num // 100 % 10
+m = num // 1000 % 10
+print('===' * 8)
+print('Unidade: {}'.format(u))
+print('Dezena: {}'.format(d))
+print('Centena: {}'.format(c))
+print('Milhar: {}'.format(m))
