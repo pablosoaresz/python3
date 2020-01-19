479943906e3bedc7586b94c9a922553b1907d729
pablosoaresz <pabloetf@gmail.com>
2020-01-18 20:20:47 -0400

    aulas dia 18/01/2020

--- PythonExercicios/exe022.py
+++ PythonExercicios/exe022.py
@@ -3,3 +3,12 @@
 #2- O nome com todas minúsculas
 #3- Quantas letras ao todo (sem considerar espaços)
 #4- Quantas letras tem o primeiro nome
+
+nome = str(input('Digite seu nome, usando letras maiúsculas e minúsculas: ')).strip()
+print('Analisando...')
+print('Seu nome é {} em letras maiúsculas.'.format(nome.upper()))
+print('Seu nome é {} em letras minúsculas.'.format(nome.lower()))
+print('Seu nome completo possui {} letras.'.format(len(nome) - nome.count(' ')))
+#print('Seu primeiro nome tem {} letras.'.format(nome.find(' ')))
+separa = nome.split()
+print('Seu primeiro nome é {}, e possui {} letras.'.format(separa[0].capitalize(), len(separa[0])))
