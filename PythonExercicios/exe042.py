ac67dcef0cdd63b0ccc60df3ad85e0b04d370c72
pablosoaresz <pabloetf@gmail.com>
2020-01-24 00:35:55 -0400

    teoria da aula #12, e add 10 desafios.

---
+++ PythonExercicios/exe042.py
@@ -0,0 +1,2 @@
+# Refaça o DESAFIO 035 dos triângulos, acrescentando o rescurso de mostrar que tipo de triângulo será formado:
+# -Equilátero: todos os lados iguais; -Isósceles: dois lados iguais; -Escaleno: todos os lados diferentes.
---
+++ pythonteste/aula012a.py
@@ -0,0 +1,10 @@
+nome = str(input('Qual é seu nome? '))
+if nome == 'Pablo':
+    print('Que nome bonito!')
+elif print == 'Pedro' or nome == 'Maria' or nome == 'Patricia':
+    print('Seu nome é bem popular no Brasil.')
+elif nome in 'Ana Cláudia Jéssica Juliana':
+    print('Belo nome feminino')
+#else:
+    # print('Seu nome, {} é bem normal!'.format(nome))
+print('Bom dia, {}!'.format(nome))
